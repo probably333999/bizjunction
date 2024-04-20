003157c9d8f84d3b89167228c91f4d8f6e446669
probably333999 <90316671+probably333999@users.noreply.github.com>
2024-04-20 10:53:23 +0500

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -7,6 +7,6 @@ app.get('/', (req, res) => {
   res.send('Server is alive!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(3001, () => {
+    console.log('Server is running on port 3001');
 });
